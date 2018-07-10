65639ee5cef649a65998c451890990793769e759
guoxinyuan <156060608792@163.com>
2018-07-10 21:47:48 +0800

    [更新]取消正式版,更名https-axios,发布0.1.0非正式版本

--- src/http.js
+++ src/http.js
@@ -1,55 +1,51 @@
 import axios from 'axios'
 import promise from 'es6-promise';
 
-export const STATUS_SUCCESS = 200
+const STATUS_SUCCESS = 200
+const defaultHeader = {'accept': 'application/json; charset=utf-8'}
 
 promise.polyfill();
 
-const http = () => {
-    const defaultHeader = {'accept': 'application/json; charset=utf-8'}
+const http = {
+    POST: (url, data, context, action, header = defaultHeader) => {
+        console.log(header)
+        axios({
+            method: 'post',
+            url: url,
+            data: data,
+            headers: header
+        })
+            .then((res) => {
+                if (res['status'] === STATUS_SUCCESS) {
+                    context.onRealSuccess ?
+                        context.onRealSuccess(res['data'], action, data)
+                        : context.onSuccess(res['data'], action, data)
 
-    return {
-
-        POST: (url, data, context, action, header = defaultHeader) => {
-            console.log(header)
-            axios({
-                method: 'post',
-                url: url,
-                data: data,
-                headers: header
+                }
+                else {
+                    context.onFailure(res['data'], action)
+                }
             })
-                .then((res) => {
-                    if (res['status'] === STATUS_SUCCESS) {
-                        context.onRealSuccess ?
-                            context.onRealSuccess(res['data'], action, data)
-                            : context.onSuccess(res['data'], action, data)
-
-                    }
-                    else {
-                        context.onFailure(res['data'], action)
-                    }
-                })
-                .catch((e) => {
-                    try {
-                        context.onError(e.response.status, action, data)
-                    } catch (exception) {
-                        throw e
-                    }
-                })
-        },
-
-        GET: (url, success, error, action) => {
-            axios.get(url).then((res) => {
-                success(res, action)
-            }).catch((e) => {
+            .catch((e) => {
                 try {
-                    error ? error(e.response.status, action) : null
+                    context.onError(e.response.status, action, data)
                 } catch (exception) {
                     throw e
                 }
-
             })
-        }
+    },
+
+    GET: (url, success, error, action) => {
+        axios.get(url).then((res) => {
+            success(res, action)
+        }).catch((e) => {
+            try {
+                error ? error(e.response.status, action) : null
+            } catch (exception) {
+                throw e
+            }
+
+        })
     }
 }
-export default http()
+export default http
